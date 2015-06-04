266e527b1fe34b8a17a36f30746af4092f93866e
ankita-ks <kumariankita002@gmail.com>
2015-06-05 02:40:19 +0530

    added the button display code

--- modules/ext.LanguageTool.js
+++ modules/ext.LanguageTool.js
@@ -4,7 +4,17 @@
 	 * @class mw.languageTool
 	 * @singleton
 	 */
-	mw.languageTool = {
-	};
-
+	mw.languageTool = function VeUiMWLanguageTool( toolGroup, config ) {
+			ve.ui.DialogTool.call( this, toolGroup, config );
+		};
+	OO.inheritClass( ve.ui.MWLanguageTool, ve.ui.DialogTool );
+	ve.ui.MWLanguageTool.static.name = 'Language';
+	ve.ui.MWLanguageTool.static.group = 'object';
+	ve.ui.MWLanguageTool.static.icon = 'picture';
+	ve.ui.MWLanguageTool.static.title = 'LanguageTool';
+	//ve.ui.MWLanguageTool.static.modelClasses = [ ve.dm.MWBlockImageNode, ve.dm.MWInlineImageNode ];
+	//ve.ui.MWLanguageTool.static.commandName = 'media';
+	//ve.ui.MWLanguageTool.static.autoAddToCatchall = false;
+	//ve.ui.MWLanguageTool.static.autoAddToGroup = false;
+	ve.ui.toolFactory.register( ve.ui.MWLanguageTool );
 }() );
