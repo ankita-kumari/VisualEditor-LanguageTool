5ea95fa09b110f808840b1a44f7b016aab0c1911
ankita-ks <kumariankita002@gmail.com>
2015-06-05 03:27:18 +0530

    minor corrections for addition of a button

--- modules/ext.LanguageTool.js
+++ modules/ext.LanguageTool.js
@@ -7,14 +7,14 @@
 	mw.languageTool = function VeUiMWLanguageTool( toolGroup, config ) {
 			ve.ui.DialogTool.call( this, toolGroup, config );
 		};
-	OO.inheritClass( ve.ui.MWLanguageTool, ve.ui.DialogTool );
-	ve.ui.MWLanguageTool.static.name = 'Language';
-	ve.ui.MWLanguageTool.static.group = 'object';
-	ve.ui.MWLanguageTool.static.icon = 'picture';
-	ve.ui.MWLanguageTool.static.title = 'LanguageTool';
-	//ve.ui.MWLanguageTool.static.modelClasses = [ ve.dm.MWBlockImageNode, ve.dm.MWInlineImageNode ];
-	//ve.ui.MWLanguageTool.static.commandName = 'media';
-	//ve.ui.MWLanguageTool.static.autoAddToCatchall = false;
-	//ve.ui.MWLanguageTool.static.autoAddToGroup = false;
+	OO.inheritClass( mw.languageTool, ve.ui.DialogTool );
+	mw.languageTool.static.name = 'Language';
+	mw.languageTool.static.group = 'object';
+	mw.languageTool.static.icon = 'picture';
+	mw.languageTool.static.title = 'LanguageTool';
+	//mw.languageTool.static.modelClasses = [ ve.dm.MWBlockImageNode, ve.dm.MWInlineImageNode ];
+	//mw.languageTool.static.commandName = 'media';
+	//mw.languageTool.static.autoAddToCatchall = false;
+	//mw.languageTool.static.autoAddToGroup = false;
 	ve.ui.toolFactory.register( mw.languageTool );
 }() );
