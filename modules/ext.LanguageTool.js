70bb2741658f1e8e326490c1432d90e09436ea66
ankita-ks <kumariankita002@gmail.com>
2015-06-06 13:03:41 +0530

    Added action and command file templates

--- modules/ext.LanguageTool.js
+++ modules/ext.LanguageTool.js
@@ -9,9 +9,42 @@
 		};
 	OO.inheritClass( mw.languageTool, ve.ui.DialogTool );
 	mw.languageTool.static.name = 'Language';
-	mw.languageTool.static.group = 'object';
+	mw.languageTool.static.group = 'meta';
 	mw.languageTool.static.icon = 'picture';
 	mw.languageTool.static.title = 'LanguageTool';
+	/**
+	 *Function to extract text from VisualEditor
+	 */
+	function extractText(){
+		var nodes = [];
+		var model = ve.init.target.getSurface().getModel();
+		function getTextNodes( obj ) {
+			var i;
+ 
+			for ( i = 0; i < obj.children.length; i++ ) {
+				if ( obj.children[i].type == 'text'){
+					nodes.push(obj.children[i]);
+				}
+ 
+				if ( obj.children[i].children ) {
+					getTextNodes( obj.children[i] );
+				}
+			}
+		}
+		getTextNodes(ve.init.target.getSurface().getModel().getDocument().getDocumentNode());
+		return nodes;
+	}
+	function sendText(){
+		var textNodes = extractText();
+		for (var nodeI = 0; nodeI < textNodes.length; nodeI++) {
+			var node = textNodes[nodeI];
+			var nodeRange = node.getRange();
+			var nodeText = model.getLinearFragment(nodeRange).getText();
+			console.log(nodeText);
+		}
+	}
+ 
+
 	//mw.languageTool.static.modelClasses = [ ve.dm.MWBlockImageNode, ve.dm.MWInlineImageNode ];
 	//mw.languageTool.static.commandName = 'media';
 	//mw.languageTool.static.autoAddToCatchall = false;
---
+++ modules/ext.LanguageToolAction.js
@@ -0,0 +1,87 @@
+( function () {
+/*!
+ * VisualEditor UserInterface LanguageToolAction class.
+ *
+ * @copyright 2011-2015 VisualEditor Team and others; see http://ve.mit-license.org
+ */
+
+/**
+ * LanguageTool action.
+ *
+ * @class
+ * @extends ve.ui.Action
+ *
+ * @constructor
+ * @param {ve.ui.Surface} surface Surface to act on
+ */
+mw.languageToolAction = function VeUiLanguageToolAction( surface ) {
+	// Parent constructor
+	ve.ui.Action.call( this, surface );
+};
+
+/* Inheritance */
+
+OO.inheritClass( mw.languageToolAction, ve.ui.Action );
+
+/* Static Properties */
+
+mw.languageToolAction.static.name = 'LanguageTool';
+
+/**
+ * List of allowed methods for the action.
+ *
+ * @static
+ * @property
+ */
+mw.languageToolAction.static.methods = [ 'extract', 'send' ];
+
+/* Methods */
+
+/**
+ * Step backwards in time.
+ *
+ * @method
+ * @return {boolean} Action was executed
+ */
+mw.languageToolAction.prototype.extract = function () {
+		var nodes = [];
+		var model = ve.init.target.getSurface().getModel();
+		function getTextNodes( obj ) {
+			var i;
+ 
+			for ( i = 0; i < obj.children.length; i++ ) {
+				if ( obj.children[i].type == 'text'){
+					nodes.push(obj.children[i]);
+				}
+ 
+				if ( obj.children[i].children ) {
+					getTextNodes( obj.children[i] );
+				}
+			}
+		}
+		getTextNodes(ve.init.target.getSurface().getModel().getDocument().getDocumentNode());
+		return nodes;
+	}
+
+/**
+ * Step forwards in time.
+ *
+ * @method
+ * @return {boolean} Action was executed
+ */
+mw.languageToolAction.prototype.send = function () {
+		var textNodes = extractText();
+		for (var nodeI = 0; nodeI < textNodes.length; nodeI++) {
+			var node = textNodes[nodeI];
+			var nodeRange = node.getRange();
+			var nodeText = model.getLinearFragment(nodeRange).getText();
+			console.log(nodeText);
+		}
+		return true;
+	}
+
+/* Registration */
+
+ve.ui.actionFactory.register( mw.languageToolAction );
+
+}() );
